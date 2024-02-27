6271185db0d2d48a9299a5ed65a192d166b809bd
aldozavala <160289969+aldozavala@users.noreply.github.com>
2024-02-27 00:54:11 +0000

    Version 1

---
+++ include/Mascota.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include<Alimento.hpp>
+class Mascota
+{
+    private:
+    int Felicidad;
+    int Energia;
+     public:
+     Mascota()
+     {
+        this->Energia=0;
+     }
+     ~Mascota() {}
+        void Comer (Alimento alimento)
+        {
+            this->Energia +=
+            Alimento.ExtraerEnergia();
+        }
+        int LeerEnergia(){
+            return this->Energia;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include<Mascota.hpp>
+#include<Alimento.hpp>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"Hola "<<"REGISTRO"<<endl;
+    Mascota m1;
+    Mascota m2;
+    Mascota m3;
+        Alimento alimento(5);
+        m1.Comer(alimento);
+        m2.Comer(alimento);
+        m3.Comer(alimento);
+    cout<<"Mascota "<<m1.LeerEnergia()<<endl;
     return 0;
 }
